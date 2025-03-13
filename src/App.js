7fdee9c0f862f6af1ba1f6eec72aa0597b927e55
kheira <ouicikheira770@gmail.com>
2025-03-13 09:11:02 +0100

    deploie

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
+import Header from './components/Header.js'
 
+const App = ()=> {
 
-
-import Header from "./components/header";
-const App =()=>{
   return (
-  <Header>
-     <nav class="navbar bylight">
-  
-  <div class="container-fluid">
 
-   <article class="cd-md-5 text-light bg-dark">
+    <Header />
 
-    <p class="text-light bg-dark">
+  )
 
-    <span>john doe</span>
-
-    </p>
-
-    <a  class="navbar-brand" href="./pages/HOME.html">HOME</a>
-
-    <a  class="navbar-brand" href="./pages/offresrrv.html">Service</a>
-
-    <a  class="navbar-brand" href="./pages/portfolio.html"> Portfolio</a>
-
-    <a  class="navbar-brand" href="./pages/contact.html">contact</a>
-
-    <a  class="navbar-brand" href="./pages/mentionleg1.html">mention légale</a>
-
-    <a  class="navbar-brand" href="./pages/profilgithub.html">Profil github</a>
-
-  </article>
-
-    </div>
-
-</nav>
-  </Header>)
-  
 }
 
-export default App;
+
+export default App
